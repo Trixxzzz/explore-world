6df7c0c2bf71c40a4e5001f66cb3d81ddf1ed79d
Trixxzzz <ramtekearyan2003@gmail.com>
2023-07-21 20:34:20 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const  Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>ExploreWorld</h1>
+            <p>@all right reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <a href="https://www.linkedin.com/in/ramteke-aryan-ratnaghosh-79334422a/" target={"blank"}>LinkendIn</a>
+            <a href="https://instagram.com/its_.aryannn._?igshid=MzNlNGNkZWQ4Mg==" target={"blank"}>Instagram</a>
+            <a href="https://github.com/Trixxzzz" target={"blank"}>Github</a>
+        </div>
+    </footer>
+  )
+}
+
+export default  Footer
